376134ae9e490e54025ae9164437cba781a6dd03
Gustavo Silva <gjbs1903@gmail.com>
2025-02-01 07:02:07 -0300

    improve updateEventAvailability function

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -21,36 +21,35 @@ class Reservation extends Model
 
     public static function boot(): void
     {
+        parent::boot();
+
         static::creating(function (self $reservation) {
-            if ($reservation->number_of_tickets > $reservation->event->availability) {
+            if ($reservation->number_of_tickets > $reservation->event->total_availability) {
                 $ticketsLabel = str('ticket')
-                    ->plural($reservation->event->availability)
+                    ->plural($reservation->event->total_availability)
                     ->toString();
 
                 throw new Exception(
-                    $reservation->event->availability
-                        ? "This event has only {$reservation->event->availability} {$ticketsLabel} remaining."
+                    $reservation->event->total_availability
+                        ? "This event has only {$reservation->event->total_availability} {$ticketsLabel} remaining."
                         : "There are no tickets available for this event."
                 );
             }
         });
 
         static::created(function (self $reservation) {
-            $reservation
-                ->event()
-                ->decrement(
-                    column: 'availability',
-                    amount: $reservation->number_of_tickets
-                );
+            $reservation->updateEventAvailability($reservation->event);
         });
 
         static::deleted(function (self $reservation) {
-            $reservation->event()
-                ->increment(
-                    column: 'availability',
-                    amount: $reservation->number_of_tickets
-                );
+            $reservation->updateEventAvailability($reservation->event);
         });
     }
 
+    private function updateEventAvailability(Event $event)
+    {
+        $event->update([
+            'remaining_availability' => $event->total_availability - $event->reservations()->sum('number_of_tickets')
+        ]);
+    }
 }
